1f76c63ecbe09fd578dd6ecddfde183df7989e72
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2022-12-21 08:48:54 +0530

    Add files via upload
    Java program to show variable Arguments.

---
+++ varargs.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class varargs
+{
+    	static void fun(int... a){
+		System.out.println("Number of arguments: "+ a.length);
+		for (int i : a)
+		System.out.print(i);
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		fun(100);
+		fun(1, 2, 3, 4);
+		fun();
+	}
+}
